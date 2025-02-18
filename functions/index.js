07950fe2fd33021a33839197f6efe83a62fb2d62
Coding Rooms <git@codingrooms.io>
2025-02-18 02:23:27 +0000

    Deploy to firebase

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,13 @@
 const {onRequest} = require("firebase-functions/v2/https");
 
 exports.randomNumber = onRequest((request, response) => {
+    {cors : [/flutterflow\.app$/,/flutterflow\.io$/,]}
     const randomNumber = Math.random();
+
+    const randomNumBetween1And100 = Math.floor(Math.random() * 100) + 1; 
+    console.log("Random number between 1 and 100:", randomNumBetween1And100);
+
     console.log("Random number:", randomNumber);
-    response.send(randomNumber.toString());
+    console.log("Hello world");
+    response.send(randomNumberBetween1And100.toString());
 });
